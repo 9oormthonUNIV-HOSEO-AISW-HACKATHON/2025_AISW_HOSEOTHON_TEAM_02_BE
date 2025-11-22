0a4a4df31791e1c3db94859f1fe5482e1cbc10aa
Ahnsukjoo <bos98@naver.com>
2025-11-22 19:14:05 +0900

    intitializer

---
+++ src/main/java/com/backend/dosol/DosolApplication.java
@@ -0,0 +1,13 @@
+package com.backend.dosol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/dosol/DosolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.dosol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
